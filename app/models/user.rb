d72f8abd2139d1fddff619fca3c6796ff029e0e2
Rita <rita.lu@yahoo.com.tw>
2018-03-20 23:42:17 +0800

    slove confict in controller & model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def is_admin?
-    self.id == 1
+    self.id == 1 || Rails.env.development?
   end
-  #id是1的User就是Admin
+  #id是1的User就是Admin & 在開發環境中，所有 User 都是 admin
 end
